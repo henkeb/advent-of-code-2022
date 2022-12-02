dd5a58303d0a620c8f19f4bc6a04a03f1bd8647e
Henrik Berg <khp.berg@gmail.com>
2022-12-02 13:43:27 +0100

    Solved day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,1 +1,115 @@
-pub fn calculate(slice: &str) {}
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq, Eq)]
+enum Sign {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+enum MatchResults {
+    Win,
+    Draw,
+    Loss,
+}
+
+impl MatchResults {
+    fn value(&self) -> i32 {
+        match *self {
+            MatchResults::Win => 6,
+            MatchResults::Draw => 3,
+            MatchResults::Loss => 0,
+        }
+    }
+}
+
+impl Sign {
+    fn value(&self) -> i32 {
+        match *self {
+            Sign::Rock => 1,
+            Sign::Paper => 2,
+            Sign::Scissors => 3,
+        }
+    }
+}
+
+impl FromStr for Sign {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Sign::Rock),
+            "B" | "Y" => Ok(Sign::Paper),
+            "C" | "Z" => Ok(Sign::Scissors),
+            _ => Err(()),
+        }
+    }
+}
+
+pub fn calculate(slice: &str) -> Result<(i32, i32), ()> {
+    Ok(slice
+        .trim()
+        .lines()
+        .map(|round| round.split_once(" ").unwrap())
+        .fold((0, 0), |(acc_part1, acc_part2), (elf, you)| {
+            let elf_1 = Sign::from_str(elf).unwrap();
+            let you_2 = Sign::from_str(you).unwrap();
+            (
+                acc_part1 + points_per_round(&elf_1, &you_2).unwrap(),
+                acc_part2 + points_per_round_part2(&elf_1, &you_2).unwrap(),
+            )
+        }))
+}
+
+fn points_per_round(elf: &Sign, you: &Sign) -> Result<i32, ()> {
+    let point = match (elf, you) {
+        (Sign::Rock, Sign::Paper) => MatchResults::Win.value(),
+        (Sign::Rock, Sign::Scissors) => MatchResults::Loss.value(),
+        (Sign::Paper, Sign::Rock) => MatchResults::Loss.value(),
+        (Sign::Paper, Sign::Scissors) => MatchResults::Win.value(),
+        (Sign::Scissors, Sign::Rock) => MatchResults::Win.value(),
+        (Sign::Scissors, Sign::Paper) => MatchResults::Loss.value(),
+        (elf, you) if elf == you => MatchResults::Draw.value(),
+        _ => return Err(()),
+    };
+    Ok(point + you.value())
+}
+
+fn points_per_round_part2(elf: &Sign, you: &Sign) -> Result<i32, ()> {
+    match (elf, you) {
+        (Sign::Rock, Sign::Rock) => Ok(MatchResults::Loss.value() + Sign::Scissors.value()),
+        (Sign::Paper, Sign::Rock) => Ok(MatchResults::Loss.value() + Sign::Rock.value()),
+        (Sign::Scissors, Sign::Rock) => Ok(MatchResults::Loss.value() + Sign::Paper.value()),
+        (elf, Sign::Paper) => Ok(MatchResults::Draw.value() + elf.value()),
+        (Sign::Rock, Sign::Scissors) => Ok(MatchResults::Win.value() + Sign::Paper.value()),
+        (Sign::Paper, Sign::Scissors) => Ok(MatchResults::Win.value() + Sign::Scissors.value()),
+        (Sign::Scissors, Sign::Scissors) => Ok(MatchResults::Win.value() + Sign::Rock.value()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calculate_point() {
+        let input: &str = "A Y
+B X
+C Z";
+        assert_eq!(calculate(&input), Ok((15, 12)));
+    }
+
+    #[test]
+    fn calculate_rock_win() {
+        assert_eq!(points_per_round(&Sign::Scissors, &Sign::Rock), Ok(7));
+    }
+}
+
+// First implementation of part 1 below
+//pub fn calculate(slice: &str) -> Result<i32, ()> {
+//slice
+//.trim()
+//.split("\n")
+//.map(|round| round.split(" ").collect::<Vec<&str>>())
+//.map(|line| points_per_round(&Sign::from_str(line[0])?, &Sign::from_str(line[1])?))
+//.sum()
+//}
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use advent_of_code_2022::day2;
 fn main() -> Result<(), ExitFailure> {
     let args: Vec<String> = env::args().collect();
     if let Ok(input) = fs::read_to_string(&args[1]) {
-        println!("{:?}", day2::calculate(&input))
+        println!("{:?}", day2::calculate(&input).unwrap())
     } else {
         println!("ERROR")
     }
