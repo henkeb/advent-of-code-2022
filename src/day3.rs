5c2ed2f3864a132db3031ef2913d71c767bfb4aa
Henrik Berg <khp.berg@gmail.com>
2022-12-05 09:09:07 +0100

    day 3 solved!

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,73 @@
-pub fn calculate(_slice: &str) -> Result<i32, ()> {
-    unimplemented!();
+// Rewritten into functional style which i want to learn. HEAVILY inspired by
+// https://github.com/markus-k/adventofcode/blob/main/2022/day03/src/main.rs
+
+pub fn calculate(slice: &str) -> i32 {
+    slice
+        .lines()
+        .filter_map(|line| common_chars(split_rucksack(line)))
+        .map(get_character_value)
+        .sum()
+}
+
+fn split_rucksack(rucksack: &str) -> (&str, &str) {
+    rucksack.split_at(rucksack.len() / 2)
+}
+
+fn common_chars(rucksack_sides: (&str, &str)) -> Option<char> {
+    rucksack_sides
+        .0
+        .chars()
+        .find(|ch| rucksack_sides.1.contains(*ch))
+}
+
+fn get_character_value(ch: char) -> i32 {
+    match ch {
+        small if small.is_lowercase() => small as i32 - 96,
+        capital if capital.is_uppercase() => capital as i32 - 65 + 27,
+        _ => 0,
+    }
+}
+
+pub fn calculate_part2(slice: &str) -> i32 {
+    slice
+        .lines()
+        .collect::<Vec<_>>()
+        .chunks_exact(3)
+        .filter_map(find_common_char)
+        .map(get_character_value)
+        .sum()
+}
+
+fn find_common_char(lines: &[&str]) -> Option<char> {
+    lines
+        .first()?
+        .chars()
+        .find(|ch| lines[1..].iter().all(|shared| shared.contains(*ch)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calculate_priority() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+        assert_eq!(calculate(&input), 157);
+    }
+
+    #[test]
+    fn calculate_priority_part2() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+        assert_eq!(calculate_part2(&input), 70);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@ use exitfailure::ExitFailure;
 use std::env;
 use std::fs;
 
-use advent_of_code_2022::day3;
-
 fn main() -> Result<(), ExitFailure> {
     let args: Vec<String> = env::args().collect();
     if let Ok(input) = fs::read_to_string(&args[1]) {
-        println!("{:?}", day3::calculate(&input).unwrap())
+        println!("{:?}", advent_of_code_2022::day3::calculate(&input))
     } else {
         println!("ERROR")
     }
