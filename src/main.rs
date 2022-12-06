af4039d093c11f8e565349c6a4fffc2c50239f79
Henrik Berg <khp.berg@gmail.com>
2022-12-06 13:16:42 +0100

    fixing main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
-use exitfailure::ExitFailure;
 use std::env;
 use std::fs;
 
-fn main() -> Result<(), ExitFailure> {
+fn main() {
     let args: Vec<String> = env::args().collect();
     if let Ok(input) = fs::read_to_string(&args[1]) {
-        println!("{:?}", advent_of_code_2022::day6::calculate(&input, 14))
+        match args[1].split(&['/', '.']).nth(1).unwrap() {
+            "day1" => println!(
+                "Part1: {}\nPart2: {}",
+                advent_of_code_2022::day1::calculate_calories(&input),
+                advent_of_code_2022::day1::calculate_calories_top_three(&input)
+            ),
+            "day2" => println!(
+                "(Part1, Part2): {:?}",
+                advent_of_code_2022::day2::calculate(&input).unwrap(),
+            ),
+            "day3" => println!(
+                "Part1: {}\nPart2: {}",
+                advent_of_code_2022::day3::calculate(&input),
+                advent_of_code_2022::day3::calculate_part2(&input)
+            ),
+            "day4" => println!(
+                "Part1: {}\nPart2: {}",
+                advent_of_code_2022::day4::calculate(&input),
+                advent_of_code_2022::day4::calculate_2(&input)
+            ),
+            "day5" => println!(
+                "Part1: {}\nPart2: {}",
+                advent_of_code_2022::day5::calculate(&input, false),
+                advent_of_code_2022::day5::calculate(&input, true)
+            ),
+            "day6" => println!(
+                "Part1: {}\nPart2: {}",
+                advent_of_code_2022::day6::calculate(&input, 4).unwrap(),
+                advent_of_code_2022::day6::calculate(&input, 14).unwrap()
+            ),
+            day => println!("Implementation does not exist for: {}", day),
+        }
     } else {
-        println!("ERROR")
+        println!("Could not read input file");
     }
-    Ok(())
 }
